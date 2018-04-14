ce417fe439133f7bcf16c1a15fef97ac5157a490
Pawel Urbanski <pawel@clusterone.ca>
2018-04-14 08:37:36 -0600

    number of tags

--- process.php
+++ process.php
@@ -2,13 +2,12 @@
 if(!empty($_POST)){
 
 
-$lead_data["url"] = $_POST['url'];
 $lead_data["post"] = isset($_POST['post']) ? $_POST['post'] : "";
 $lead_data["tags"] = isset($_POST['tags']) ? $_POST['tags'] : "";
-$lead_data["maxHashtags"] = isset($_POST['maxHashtags']) ? $_POST['maxHashtags'] : "2";
+$lead_data["maxHashtags"] = !empty($_POST['maxHashtags']) ? $_POST['maxHashtags'] : 2;
 
 $lead_data["client_id"] = "c4e432c37799f525e8f55d5f31337cc4f3845c508d55";
-$url = $lead_data["url"] ."?". http_build_query($lead_data);
+$url = $_POST['url'] ."?". http_build_query($lead_data);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
